3e89940cd2e2da29b00439ec78e9c93e52bffd18
eunahpae <eunahp86@gmail.com>
2023-11-07 22:11:17 +0900

    use state

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState('동네 맛집 추천');
+  let [글제목2, 글제목변경2] = useState('노원 맛집 추천');
+  let [글제목3, 글제목변경3] = useState('서울 맛집 추천','동네 우동 맛집');
+
   let posts = '서울시 노원구 공릉동';
   // let posts = { color : 'blue', fontSize : '30px' }
   
-  function 함수(){
-    return 100
-  }
+  // function 함수(){
+  //   return 100
+  // }
 
   return (
     <div className="App">
@@ -28,7 +33,14 @@ function App() {
         <div>개발 Blog</div>
       </div> 
       <div className="list">
-        <h3> { posts }</h3>
+        {/* <h3> { posts }</h3> */}
+        <h3> { 글제목 }</h3>
+        <p>11월 7일 발행</p>
+        <hr/>
+        <h3> { 글제목2 }</h3>
+        <p>11월 7일 발행</p>
+        <hr/>
+        <h3> { 글제목3 }</h3>
         <p>11월 7일 발행</p>
         <hr/>
       </div>
