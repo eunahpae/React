56b07db1df8206ef3e1227501dd9a18757458288
eunahpae <eunahp86@gmail.com>
2023-11-16 15:46:00 +0900

    1116(2)

--- blog/src/App.js
+++ blog/src/App.js
@@ -10,35 +10,17 @@ function App() {
   let [좋아요, 좋아요변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
-
-  // function 제목바꾸기(){
-  //   var newArray = [...글제목];
-  //   newArray[0] = '집근처 맛집 추천';
-  //   글제목변경( newArray );
-  // }
+  function 글수정(){
+    let copy = [...글제목];
+    copy[0] = '집근처 맛집 추천';
+    글제목변경(copy);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      {/* <button onClick={ 제목바꾸기 }>변경</button> */}
-      {/* <div className="list">
-        <h3 onClick={() => { setModal(!modal) }}> {글제목[0]} <span onClick={() => { 좋아요변경(좋아요 + 1) }}>👍</span> {좋아요} </h3>
-        <p>11월 5일 발행</p>
-        <hr />
-      </div> */}
-      {/* <button onClick={()=>{}}>버튼</button> */}
-      {/* <div className="list">
-        <h3> {글제목[1]} </h3>
-        <p>11월 6일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3> {글제목[2]} </h3>
-        <p>11월 7일 발행</p>
-        <hr />
-      </div > */}
 
       {
         글제목.map(function (a, i) {
@@ -60,19 +42,22 @@ function App() {
       }
 
       {
-        modal == true ? <Modal /> : null
+        modal == true ? <Modal 글수정={글수정} title={글제목}/> : null
       }
 
     </div >
   );
 }
 
-function Modal() {
+
+
+function Modal(props) {
   return (
     <div className='modal'>
-      <h2>제목</h2>
+      <h2>{props.title[0]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.글수정}>글수정</button>
     </div>
   )
 }
